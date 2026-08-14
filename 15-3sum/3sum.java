ebee59650c49330fba56e23585a61ebfad96b799
Faizan Alam <faizanalam0501@gmail.com>
2026-08-14 12:19:47 +0530

    Time: 33 ms (72.77%) | Memory: 60.4 MB (7.54%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,43 +4,39 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
-        Arrays.sort(nums);
         int n=nums.length;
+        Arrays.sort(nums);
         for(int i=0;i<n-2;i++){
-            if(i > 0 && nums[i] == nums[i-1]) {
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
             int left=i+1;
             int right=n-1;
+            int target=-1*nums[i];
+            
             while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
+                if(nums[left]+nums[right]==target){
                     List<Integer> triplets=new ArrayList<>();
                     triplets.add(nums[i]);
                     triplets.add(nums[left]);
                     triplets.add(nums[right]);
                     result.add(triplets);
-                    while(left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while(left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
                     left++;
                     right--;
+                    while(left<n&&nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(right>=0&&nums[right]==nums[right+1]){
+                        right--;
+                    }
                 }
-                else if(sum<0){
+                else if(nums[left]+nums[right]<target){
                     left++;
                 }
                 else{
                     right--;
                 }
-
-
-                
             }
-            
-            
         }
         return result;
         
