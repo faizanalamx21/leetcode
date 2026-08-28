91addd4faf468d6e23602a1bda0769c4e6e059ed
Faizan Alam <faizanalam0501@gmail.com>
2026-08-28 14:41:43 +0530

    Time: 8 ms (91.63%) | Memory: 48.9 MB (85.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] a) {
+        Arrays.sort(a, (x, y) -> x[0] - y[0]);//array ko start k basis p sort krnege
 
-        Arrays.sort(a, (x, y) -> x[0] - y[0]);
-
-        List<int[]> res = new ArrayList<>();
-
-        int start = a[0][0];
-        int end = a[0][1];
-
-        for (int i = 1; i < a.length; i++) {
-
-            if (end >= a[i][0]) {
-                end = Math.max(end, a[i][1]);
-            } 
-            else {
-                res.add(new int[]{start, end});
-
-                start = a[i][0];
-                end = a[i][1];
+        List<int[]> result=new ArrayList<>();
+        int start1=a[0][0];//pehla interval
+        int end1=a[0][1];
+        for(int i=1;i<a.length;i++){
+            int start2=a[i][0];
+            int end2=a[i][1];
+            if(end1>=start2){//merge kara denge
+                start1=start1;
+                end1=Math.max(end1,end2);
+                continue;
+            }
+            else{
+                result.add(new int[]{start1,end1});
+                start1=start2;
+                end1=end2;
             }
         }
+        result.add(new int[]{start1,end1});
+        return result.toArray(new int[result.size()][]);
 
-        res.add(new int[]{start, end});
-
-        return res.toArray(new int[res.size()][]);
     }
 }
