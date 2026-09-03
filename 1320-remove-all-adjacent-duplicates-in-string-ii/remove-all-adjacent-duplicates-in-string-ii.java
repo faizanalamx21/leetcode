c2ab01ae1d307c813725b4e0f97d4e3a7c142688
Faizan Alam <faizanalam0501@gmail.com>
2026-09-03 11:03:30 +0530

    Time: 28 ms (60.97%) | Memory: 47.4 MB (37.59%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<int[]> st=new Stack<>();
+        Stack<int[]> st=new Stack<>();//stack k har element as an array rhega jo character aur uski frequesny ko store krega
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(!st.isEmpty()&&st.peek()[0]==ch){
-                if(st.peek()[1]==k-1){
+                if(st.peek()[1]==k-1){//agar frequesncy k-1 hojye to pop kr denge
                     st.pop();
                 }
                 else{
-                    st.peek()[1]++;
+                    st.peek()[1]++;//wrna frequesncy ek badha denge 
                 }
             }
             else{
-                st.push(new int[]{ch,1});
+                st.push(new int[]{ch,1});//aagar character match hi na krey to usko push krenge with initial frequency of 1
             }
         }
         StringBuilder ans = new StringBuilder();
 
-        for (int[] curr : st) {
+        for (int[] curr : st) {//stack m char aur uski frequesncy saath m hogi to usko normal string k form m krnwey k liye ye kr rhey h
             for (int i = 0; i < curr[1]; i++) {
-                ans.append((char) curr[0]);
+                ans.append((char) curr[0]);//ye already rverse form m hi kr rha h to fr s reverse krney ki zrurat nhi h 
             }
         }
 
