1926224bf852f0921b71fdcf24177d68f86c1e3c
Faizan Alam <faizanalam0501@gmail.com>
2026-09-03 10:04:53 +0530

    Time: 13 ms (71.84%) | Memory: 48.7 MB (34.86%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
         int[]result=new int[n];
-        //result[n-1]=-1;
+        //kyuki ye circular array h to last element ka next greater _1 phley s declare nahi kr sktey
         Stack<Integer> st=new Stack<>();
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){//ismey shuru m stack m eleement push krna pdega 
             st.push(nums[i]);
         }
-        for(int i=n-1;i>=0;i--){
-            //int index=i%n;
+        for(int i=n-1;i>=0;i--){//loop last index s chlegi kyuki last index k result predefined nahi h 
+            //baaki sb next grerater k code k trah same hi h
             while(!st.isEmpty()&& st.peek()<=nums[i]){
                 st.pop();
             }
