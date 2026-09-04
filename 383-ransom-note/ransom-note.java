593b524588f37395554606a420e3d6c8ef300f8c
Faizan Alam <faizanalam0501@gmail.com>
2026-09-04 15:45:59 +0530

    Time: 20 ms (5.30%) | Memory: 47.1 MB (20.28%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<magazine.length();i++){
+        for(int i=0;i<magazine.length();i++){//pahley magazine k saarey character k frequency store krlenge 
             char c=magazine.charAt(i);
             if(!map.containsKey(c)){
                 map.put(c,1);
@@ -11,15 +11,15 @@ class Solution {
             }
         }
         for(int i=0;i<ransomNote.length();i++){
-            char c=ransomNote.charAt(i);
+            char c=ransomNote.charAt(i);//fr ransom k har character wise check krenge ki agar character map m hai aur uski frequency 0 szyada h to bs frequency 1 kam kr denge
             if(map.containsKey(c)&&map.get(c)>0){
                 map.put(c,map.get(c)-1);
             }
-            else{
+            else{//agar koi ek single character bhi nahi mila to false
                 return false;
             }
         }
-        return true;
+        return true;//wrna last m true 
         
     }
 }
