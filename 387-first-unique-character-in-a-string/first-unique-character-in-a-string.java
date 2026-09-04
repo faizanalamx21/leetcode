c532a980ddb7da6c15ec72e5cfb356a68ed5b8c7
Faizan Alam <faizanalam0501@gmail.com>
2026-09-04 15:23:51 +0530

    Time: 38 ms (8.13%) | Memory: 47.2 MB (13.36%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public int firstUniqChar(String s) {
         HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){//pehley kya krenge ki ek hasmap banayenge fr usme saari character with its frequency daaal denge 
             char c=s.charAt(i);
-            if(!map.containsKey(c)){
+            if(!map.containsKey(c)){//agar character nahi h to uskio daallenge with initial frequency of 1
                 map.put(c,1);
             }
             else{
-                map.put(c,map.getOrDefault(c,0)+1);
+                map.put(c,map.getOrDefault(c,0)+1);//wrna frequency badhaynge
             }
 
         }
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
+            char c=s.charAt(i);//fr yaha p check krenge ki jiska freuency 1 hai matlab wo unique hai 
             if(map.get(c)==1){
                 return i;
             }
