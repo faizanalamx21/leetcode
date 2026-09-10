476e61a4a101e996c2cc4d16f42e80b4af21e44c
Faizan Alam <faizanalam0501@gmail.com>
2026-09-10 12:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (57.58%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low=0;
+        int high=arr.length-1;
+        
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<arr[mid+1]){//uphill hai matlab peak right side m milega
+                low=mid+1;
+                
+            }
+            else{
+                high=mid;//down hill matlab peak left side m milega 
+            }
+            
+        }
+        
+        return low;//jab high aur low barabar hojye tb loew hi peak hoga
+
+        
+    }
+}
