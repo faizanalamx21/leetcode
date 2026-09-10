50cff8e038c8d5d9ce66d2dbc5cf37e4cd233def
Faizan Alam <faizanalam0501@gmail.com>
2026-09-10 14:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (19.93%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -2,16 +2,18 @@ class Solution {
     public int findMin(int[] nums) {
         int low=0;
         int high=nums.length-1;
-        while(low<high){
+        int res=-1;
+        while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]>nums[high]){
+            if(nums[mid]>nums[nums.length-1]){//agar mid element last elemeent s bada h matlab wo part2 ka hai to hmlog right side m check krenge
                 low=mid+1;
             }
             else{
-                high=mid;
+                res=nums[mid];//agar mid element last element s chota h to hmlog part1 m h aur fr part1 k first occurence nikalnege 
+                high=mid-1;
             }
         }
-        return nums[low];
+        return res;
         
     }
 }
