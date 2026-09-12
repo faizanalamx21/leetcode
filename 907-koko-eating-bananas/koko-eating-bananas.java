0cf3b53b3867c49c89a335afd094623858f15b49
Faizan Alam <faizanalam0501@gmail.com>
2026-09-12 12:59:26 +0530

    Time: 14 ms (38.46%) | Memory: 47.9 MB (42.70%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,24 +1,32 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low=1;
+        int low=1;//minimum eating speed 1 to rhega hi 
         int high=0;
-        for(int pile:piles){
+        int result=0;
+        for(int pile:piles){//jo max eating speed rhegi wo maximum bananas wali pile ki value hi rhegi 
             high=Math.max(high,pile);
         }
         while(low<=high){
-            int mid=low+(high-low)/2;
-            long hours=0;
-            for(int pile:piles){
-                hours+=(pile+mid-1)/mid;
+            int mid=low+(high-low)/2;//shuru me guess krney k liye mid speed s start krnege
+            long hours=0;//testcase int p fail so long ues krneg
+            for(int pile:piles){//condition k hissab s hours add krnege
+                if(pile%mid==0){
+                    hours+=pile/mid;
+                }
+                else{
+                    hours+=(pile/mid)+1;
+                }
             }
-            if(hours<=h){
-                high=mid-1;
-            }
-            else{
+            if(hours>h){//is case m speed badhani pdegi to right side jynge
                 low=mid+1;
             }
+            else{
+                result=mid;//is case m yes speed ki first occurence nikalnege kyiki ye minimum value of k maang rha h 
+                high=mid-1;
+
+            }
         }
-        return low;
+        return result;
         
     }
 }
