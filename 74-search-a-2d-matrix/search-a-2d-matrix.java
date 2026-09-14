32a5d0054303928914f9754a339fae1fdd69209d
Faizan Alam <faizanalam0501@gmail.com>
2026-09-14 13:51:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (8.83%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int low=0;//har row aur column sorted hai to har row k first value agar target s chota h to yes wrna no to pehley hm apna target row khojhney k liye last occurence of yes lagaynege
-        int high=matrix.length-1;
-        int res=-1;
+        //previous approach may fail in some interviews because it requires 2 times binary search
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        int low=0;
+        int high=(rows*cols)-1;//2D ko 1D k form m fr uska last index 
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(matrix[mid][0]<=target){
-                res=mid;//yaha hmko target row miljyega using last occurence
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        if (res == -1) {
-            return false;
-        }
-        low=0;//fr hm us row p normal binary search lagaynge kyuki wo row ek 1D sorted array k tarah behave krega 
-        high=matrix[0].length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(matrix[res][mid]==target){
+            if(matrix[mid/cols][mid%cols]==target){
                 return true;
             }
-            else if(matrix[res][mid]<target){
+            else if(matrix[mid/cols][mid%cols]<target){
                 low=mid+1;
             }
             else{
