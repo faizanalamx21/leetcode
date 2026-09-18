b769d37218033580e69dd34a2603b9d7ae5b7305
Faizan Alam <faizanalam0501@gmail.com>
2026-09-18 17:02:08 +0530

    Time: 6 ms (99.52%) | Memory: 46.4 MB (68.59%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,5 +1,5 @@
 class Solution {
-    class Pair{
+    class Pair{//Sabse pehle Heap ke liye ek Pair class banayenge, jisme String word aur uski frequency ko declare karenge
         int freq;
         String word;
         Pair(int freq,String word){
@@ -10,15 +10,15 @@ class Solution {
 
     public List<String> topKFrequent(String[] words, int k) {
         HashMap<String,Integer>map=new HashMap<>();
-        for(int i=0;i<words.length;i++){
+        for(int i=0;i<words.length;i++){//HashMap mein array ke saare words aur unki frequency ko store karenge.
             map.put(words[i],map.getOrDefault(words[i],0)+1);
         }
         PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a, b) -> {
+            (a, b) -> {//Question ki condition ke basis pe comparator define karenge. Agar frequency match nahi karti hai, toh usko frequency ke sorted order mein heap mein insert karenge. Agar frequency match karti hai, toh phir usko lexicographical order ke basis par heap mein store karenge.
                 if (a.freq != b.freq) {
-                    return a.freq - b.freq;
+                    return a.freq - b.freq;//Ye isliye karenge taaki jo chhoti frequency wala element ho, woh top par aaye aur phir usko easily hum pop kar saken.
                 }
-                return b.word.compareTo(a.word);
+                return b.word.compareTo(a.word);//Ye isliye likhe hain taaki jo alphabetically baad mein aata hai, woh pehle heap mein store ho jaaye aur top par aaye, taaki hum usko easily pop kar saken. Aur jo alphabetically pehle aata hai, woh neeche rahe, taaki woh pop out na ho jaaye.
             
             }
         );
@@ -35,7 +35,7 @@ class Solution {
         for (int i = 0; i < k; i++) {
             ans.add(pq.poll().word);
         }
-        Collections.reverse(ans);
+        Collections.reverse(ans);//kyuki hmlog minheap bnaye h aur answer ko decreasing order m chahiye
 
         return ans;
         
