c29b1263dc5318a9858f2f75044b1e9425531fab
Faizan Alam <faizanalam0501@gmail.com>
2026-09-19 13:08:36 +0530

    Time: 1 ms (99.60%) | Memory: 42.6 MB (92.59%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -2,19 +2,19 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0;i<stones.length;i++){
-            pq.add(stones[i]);
+            pq.add(stones[i]);//phley sb ko add kr denge
         }
-        while(!pq.isEmpty()||pq.size()==1){
-            if(pq.size()==1){
+        while(!pq.isEmpty()||pq.size()==1){//jbtk check krenge tbtk ya to heap khaali n hojye ya to 1 element n bechety
+            if(pq.size()==1){//agar 1 hi element bachege to usi ko return kara denge
                 return pq.peek();
             }
-            int x=pq.poll();
-            int y=pq.poll();
-            if(x==y){
+            int x=pq.poll();//1st  heavy stone
+            int y=pq.poll();//2nd heavy stone
+            if(x==y){//destroy both
                 continue;
             }
             else{
-                pq.add(Math.abs(x-y));
+                pq.add(Math.abs(x-y));//wrna dono k diffrence k absolute ko heap m wapas add krdo
             }
         }
         return 0;
